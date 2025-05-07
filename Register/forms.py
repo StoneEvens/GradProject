21893f088e362fd9fca143976353a8836f091f41
StoneEvens <steven930211@gmail.com>
2025-05-08 01:11:12 +0800

    Fixed Registration Bugs

--- Register/forms.py
+++ Register/forms.py
@@ -3,9 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    name = forms.CharField(required=True)  # You can map this to first_name or a separate profile model
+    #email = forms.EmailField(required=True)
+    #name = forms.CharField(required=True)  # You can map this to first_name or a separate profile model
 
     class Meta:
         model = User
-        fields = ("username", "email", "name", "password1", "password2")
+        #fields = ("username", "email", "name", "password1", "password2")
+        fields = ("username", "password1", "password2")
--- Register/views.py
+++ Register/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
 def index(request):
     if request.method == 'POST':
-        ##form = UserCreationForm(request.POST)
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('/home')
-
-        ##if form.is_valid():
+        form = CustomUserCreationForm(request.POST)
+        
+        if form.is_valid():
             # This creates a new User using Django’s built-in model.
-            ##user = form.save()
+            user = form.save()
             # Immediately log in the new user.
-            ##login(request, user)
-            ##return redirect('home')
+            login(request, user)
+            return redirect('home')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'register/index.html', {'form': form})
