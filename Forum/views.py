cf68eae6fe43592dcdd795e0f5fd1e0ecb4cbba0
StoneEvens <steven930211@gmail.com>
2025-05-21 18:16:28 +0800

    Fixed Security Vulnerabilities

--- Forum/views.py
+++ Forum/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def index(request, *args, **kwargs):
     return render(request, 'forum/index.html')
