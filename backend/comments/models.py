31210c651572105fac83a6fc9acc087e2f147b76
StoneEvens <steven930211@gmail.com>
2025-07-24 16:20:59 +0800

    Fixed Comments admin

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -14,6 +14,8 @@ class Comment(models.Model):
 
     postFrame = models.ForeignKey(
         PostFrame,
+        null=True,
+        blank=True,
         on_delete=models.CASCADE,
         related_name='comments'
     )
