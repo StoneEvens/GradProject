9c03029bf90fc84dafe2264f75ff63ae2b286d62
leo <leon889102@gmail.com>
2025-07-27 17:19:17 +0800

    光源改用本地

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -361,8 +361,7 @@ export default function Home() {
             }}
           >
             <color attach="background" args={['#ffca9c']} />
-            
-            <Environment preset="apartment" intensity={0.0015} />
+            <Environment files="/assets/textures/indoor_office_2k.exr" background={false} intensity={2} />
             <ambientLight intensity={0.00001} color="#fff5e6" />
             <directionalLight
               position={[0, 50, 0]}
