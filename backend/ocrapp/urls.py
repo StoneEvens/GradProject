ac7242f9fe39169de49d08dbf427ab271ed4ae87
Ivy Lee <lxzhenivyyy@gmail.com>
2025-07-28 14:26:41 +0800

    OCR功能完成

--- backend/ocrapp/urls.py
+++ backend/ocrapp/urls.py
@@ -1,14 +1,21 @@
+# from django.urls import path
+# from .views import *
+
+# urlpatterns = [
+#     path('create/', CreatePetAPIView.as_view(), name='create-pet'),
+#     path('<int:pet_id>/update/', UpdatePetAPIView.as_view(), name='update-pet'),
+#     path('illness-archives/popular-today/', TodayPopularIllnessArchiveAPIView.as_view(), name='today-popular-illness-archives'),
+#     path('my-pets/', UserPetsAPIView.as_view(), name='user_pets'),
+#     path('users/<int:pk>/archives/', UserIllnessArchiveListAPIView.as_view(), name='user-archives'),
+#     path('abnormal-posts/create/', CreateAbnormalPostAPIView.as_view(), name='create-abnormal-post'),
+#     path('symptoms/', SymptomListAPIView.as_view(), name='symptom-list'),
+#     path('my-abnormal-posts/', UserAbnormalPostListAPIView.as_view(), name='user-abnormal-posts'),
+#     path('<int:pet_id>/abnormal-posts/', PetAbnormalPostsAPIView.as_view(), name='pet-abnormal-posts'),
+# ]
+
 from django.urls import path
-from .views import *
+from .views import OCRUploadView
 
 urlpatterns = [
-    path('create/', CreatePetAPIView.as_view(), name='create-pet'),
-    path('<int:pet_id>/update/', UpdatePetAPIView.as_view(), name='update-pet'),
-    path('illness-archives/popular-today/', TodayPopularIllnessArchiveAPIView.as_view(), name='today-popular-illness-archives'),
-    path('my-pets/', UserPetsAPIView.as_view(), name='user_pets'),
-    path('users/<int:pk>/archives/', UserIllnessArchiveListAPIView.as_view(), name='user-archives'),
-    path('abnormal-posts/create/', CreateAbnormalPostAPIView.as_view(), name='create-abnormal-post'),
-    path('symptoms/', SymptomListAPIView.as_view(), name='symptom-list'),
-    path('my-abnormal-posts/', UserAbnormalPostListAPIView.as_view(), name='user-abnormal-posts'),
-    path('<int:pet_id>/abnormal-posts/', PetAbnormalPostsAPIView.as_view(), name='pet-abnormal-posts'),
+    path('upload/', OCRUploadView.as_view(), name='ocr-upload'),
 ]
