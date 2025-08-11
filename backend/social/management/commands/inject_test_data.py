26ca9ede9fd4ea426f68aaec96597c905b1563ae
StoneEvens <steven930211@gmail.com>
2025-08-12 05:11:49 +0800

    修改檔案位置

--- backend/social/management/commands/inject_test_data.py
+++ backend/social/management/commands/inject_test_data.py
@@ -11,16 +11,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = CustomUser.get_user(id=1)  # Assuming user with ID 1 exists
 
-        json_file = open('C:/Users/Steven/GradProject/backend/testdata.json', 'r', encoding='utf-8')
+        json_file = open('testdata.json', 'r', encoding='utf-8')
         test_data = json.load(json_file)
         json_file.close()
 
         print(test_data[0])
         print(test_data[0].get('content'))
 
-        for post in test_data:
-            postFrame = PostFrame.create(user = user)
-            sol_content = SoLContent.create(postFrame=postFrame, content_text=post.get('content'), location="新北市")
+        #for post in test_data:
+            #postFrame = PostFrame.create(user = user)
+            #sol_content = SoLContent.create(postFrame=postFrame, content_text=post.get('content'), location="新北市")
 
         # Your data injection logic here
         self.stdout.write(self.style.SUCCESS("Test data injected successfully"))
