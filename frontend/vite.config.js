5147a8d58b4e5c928bdca19f611b27164751e90b
StoneEvens <steven930211@gmail.com>
2025-09-05 13:39:04 +0800

    把專案移動到https上運行(包含憑證)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '140.119.19.25-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '140.119.19.25.pem')),
+    }
   },
   assetsInclude: ['**/*.glb'],
 }) 
