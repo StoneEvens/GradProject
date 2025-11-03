7b9250b2607495799c454de06e34594745ebc39f
Ivy Lee <lxzhenivyyy@gmail.com>
2025-11-03 16:19:16 +0800

    改掉新增日常貼文的步驟10

--- frontend/src/data/tutorialData.js
+++ frontend/src/data/tutorialData.js
@@ -395,7 +395,7 @@ const tutorialData = {
           position: 'textarea'
         },
         action: 'click',
-        nextCondition: 'descriptionEnterTwice'
+        nextCondition: 'pageNavigate'
       },
       {
         id: 11,
