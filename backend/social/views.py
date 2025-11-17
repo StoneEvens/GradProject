fc2bf928438d1b8754a5066ea45d3113a9240111
leo <leon889102@gmail.com>
2025-11-18 00:53:51 +0800

    debug

--- backend/social/views.py
+++ backend/social/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, status as drf_status
+from rest_framework.parsers import MultiPartParser, FormParser
 from .models import PostHashtag, PostFrame, SoLContent, PostPets, ImageAnnotation
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated
